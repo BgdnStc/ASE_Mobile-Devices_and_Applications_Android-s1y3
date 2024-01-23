dbd54fabcb2f9d7571c42591d3643d8c20217f59
unknown <bogdanstoica10@gmail.com>
2024-01-24 00:09:01 +0200

    Android (Java) - Devices and Mobile Applications seminars

--- stoica_bogdan_1105/app/src/main/java/com/example/stoica_bogdan_1105/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.stoica_bogdan_1105.utils;
-
-public interface Constants {
-    String KEY_BOOK_TITLE = "bookTitle";
-    String KEY_BOOK_AUTHOR = "bookAuthor";
-    String KEY_RELEASE_YEAR = "bookReleaseYear";
-    String KEY_BOOK = "Book";
-}
